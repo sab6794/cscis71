66c01ac7c7b5e2229b10254b549626a170a27b42
sab6794 <sab6794@g.harvard.edu>
2021-06-21 23:24:43 -0400

    First assignment

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 
 import sys
-def main():
-    args = sys.argv[1:]
 
-    print('Hello ' + args[0] + '!')
+args = sys.argv[1:]
 
-if __name__ == '__main__':
-    main()
+print('Hello ' + args[0] + '!')
